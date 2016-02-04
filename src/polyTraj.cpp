c8e897c2c05e939e33cfde64bb8ada5a68602e52
peteflorence <pete.florence@gmail.com>
2016-02-04 11:01:40 -0500

    polyTrajNode now implemented and subscribing to waypoint list topic

--- src/polyTraj.cpp
+++ src/polyTraj.cpp
@@ -1,7 +1,31 @@
 #include <ros/ros.h>
+#include <nav_msgs/Path.h>
+
+class PolyTrajNode {
+public:
+
+	PolyTrajNode(ros::NodeHandle & nh, std::string const& waypoint_topic) {
+		waypoints_sub = nh.subscribe(waypoint_topic, 10, &PolyTrajNode::OnWaypoints, this);
+	}
+
+private:
+
+	void OnWaypoints(nav_msgs::Path const& waypoints) {
+		std::cout << "Got waypoints" << std::endl;
+	}
+
+	ros::Subscriber waypoints_sub;
+};
+
+
 
 int main(int argc, char* argv[]) {
 	ros::init(argc, argv, "polyTraj_node");
+	ros::NodeHandle nh;
+
 	std::cout << "Poly traj :)" << std::endl;
-	ros::spinOnce();
+
+	PolyTrajNode poly_traj_node(nh, "/waypoint_list");
+
+	ros::spin();
 }
