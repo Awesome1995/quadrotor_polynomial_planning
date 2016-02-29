8fd968a0bb4a5866e9c5fadb970efd420f089dc4
peteflorence <pete.florence@gmail.com>
2016-02-29 11:02:40 -0500

    successfully tests for one piecewise poly with externally supplied waypoints, current velocity, and taus

--- tests/OptimalPiecewise_test.cpp
+++ tests/OptimalPiecewise_test.cpp
@@ -22,14 +22,16 @@ void testOptimalPiecewise(OptimalPiecewisePolynomialGenerator& my_optimal_piecew
     std::cout << optimal_piecewise_poly.piecewise_poly.eval(100) << std::endl;
     std::cout << optimal_piecewise_poly.piecewise_poly.eval(1000) << std::endl;
 
-    std::cout << "COSTS " << optimal_piecewise_poly.costs << std::endl;
+    std::cout << "COSTS " << optimal_piecewise_poly.costs << std::endl << std::endl;
 }
 
 void testOptimalPiecewisewithWaypoints(OptimalPiecewisePolynomialGenerator& my_optimal_piecewise_poly_generator) {
 
+    std::cout << "Testing with externally specified waypoints, current velocity, and taus" << std::endl;
+
     int n_segments = 3;
-    Eigen::VectorXd waypoints = Eigen::VectorXd(n_segments);
-    waypoints << 0.75, 0.5, 1;
+    Eigen::VectorXd waypoints = Eigen::VectorXd(n_segments+1);
+    waypoints << 0.1, 0.75, 0.95, 1.8;
     double current_velocity = 2.3;
     my_optimal_piecewise_poly_generator.setUpOptimizationWithWaypoints(waypoints, current_velocity);
 
